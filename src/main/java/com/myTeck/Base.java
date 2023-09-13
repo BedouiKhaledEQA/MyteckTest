22058bb50dfc42876611ad03dcf7a776f12dd0bb
khaledBEQA <bedouikhaled090@gmail.com>
2023-09-13 17:00:04 +0100

    First commit

---
+++ src/main/java/com/myTeck/Base.java
@@ -0,0 +1,50 @@
+package com.myTeck;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class Base {
+    public static WebDriver driver;
+    public static Properties props =new Properties();
+
+    public Base(){
+        try {
+            File file=new File("src/main/java/com/myTeck/Properties/config.Properties");
+            FileInputStream fis =new FileInputStream(file);
+            props.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void luncher(){
+        if(props.getProperty("browser").equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();}
+
+        else if(props.getProperty("browser").equalsIgnoreCase("Edge")){
+            WebDriverManager.edgedriver().setup();
+            driver=new EdgeDriver();
+            }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(6));
+        driver.get(props.getProperty("url"));
+
+
+
+        }
+
+
+}
