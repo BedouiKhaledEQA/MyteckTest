5827fd003a6dd11710d227a4be5db1cb0a3f11d8
khaledBEQA <bedouikhaled090@gmail.com>
2023-09-19 14:11:29 +0100

    First commit

---
+++ src/main/java/com/myTeck/Pages/TestPage.java
@@ -0,0 +1,21 @@
+package com.myTeck.Pages;
+
+import com.myTeck.Base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class TestPage extends Base {
+    public TestPage(){
+        PageFactory.initElements(driver,this);
+    }
+    @FindBy(xpath = "//body[1]/div[2]/div[1]/div[1]/div[2]/div[1]/nav[1]/ul[1]/li[2]/a[1]")
+    WebElement Button;
+
+    public void testperform(){
+        Button.click();
+
+    }
+
+}
