748247bd0b9d0ad3eee9e39c97828f2cfcb66ce8
khaledBEQA <bedouikhaled090@gmail.com>
2023-09-20 10:46:35 +0100

    First commit

--- src/main/java/com/myTeck/Pages/TestPage.java
+++ src/main/java/com/myTeck/Pages/TestPage.java
@@ -1,7 +1,6 @@
 package com.myTeck.Pages;
 
 import com.myTeck.Base;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
