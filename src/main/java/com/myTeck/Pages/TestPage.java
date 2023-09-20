f50e4b08249487b3a8d2f04f541b85fc397a66fe
khaledBEQA <bedouikhaled090@gmail.com>
2023-09-20 11:34:17 +0100

    First commit

--- src/main/java/com/myTeck/Pages/TestPage.java
+++ src/main/java/com/myTeck/Pages/TestPage.java
@@ -2,6 +2,7 @@ package com.myTeck.Pages;
 
 import com.myTeck.Base;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
@@ -12,12 +13,12 @@ public class TestPage extends Base {
     }
     @FindBy(xpath = "//body[1]/div[2]/div[1]/div[1]/div[2]/div[1]/nav[1]/ul[1]/li[2]/a[1]")
     WebElement Button;
-    @FindBy(id = "rw-menutop")
-    WebElement list;
 
     public void testperform() {
-        Select sel=new Select(list);
-        sel.selectByVisibleText("Service Entreprise");
+        Actions actions =new Actions(driver);
+        actions.moveToElement(Button).perform();
+        Button.click();
+
 
 
 
