cc1b43cc5e9c87b59c1f4ea9716e0db094efa3dc
khaledBEQA <bedouikhaled090@gmail.com>
2023-09-20 12:31:42 +0100

    First commit

--- src/main/java/com/myTeck/Pages/TestPage.java
+++ src/main/java/com/myTeck/Pages/TestPage.java
@@ -1,14 +1,9 @@
 package com.myTeck.Pages;
 
 import com.myTeck.Base;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class TestPage extends Base {
     public TestPage(){
@@ -17,9 +12,8 @@ public class TestPage extends Base {
     @FindBy(xpath = "//body[1]/div[2]/div[1]/div[1]/div[2]/div[1]/nav[1]/ul[1]/li[2]/a[1]")
     WebElement Button;
 
-    public void testperform() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//body[1]/div[2]/div[1]/div[1]/div[2]/div[1]/nav[1]/ul[1]/li[2]/a[1]")));
+
+    public void testperform()  {
         Button.click();
 
     }
