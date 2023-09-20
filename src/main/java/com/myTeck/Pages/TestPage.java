db31dfd9070af703f6ab78e6f49acbb791455333
khaledBEQA <bedouikhaled090@gmail.com>
2023-09-20 14:36:50 +0100

    First commit

--- src/main/java/com/myTeck/Pages/TestPage.java
+++ src/main/java/com/myTeck/Pages/TestPage.java
@@ -1,9 +1,14 @@
 package com.myTeck.Pages;
 
 import com.myTeck.Base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class TestPage extends Base {
     public TestPage(){
@@ -14,6 +19,9 @@ public class TestPage extends Base {
 
 
     public void testperform()  {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        driver.findElement(By.linkText("Connexion")).click();
+        wait.until(d -> Button.isDisplayed());
         Button.click();
 
     }
